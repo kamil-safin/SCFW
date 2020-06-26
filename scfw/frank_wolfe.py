6ecfdc1eb21368c4edf27de54008eebf37247167
kamil-safin <kamil.95@yandex.ru>
2020-06-26 22:40:29 +0300

    bound fix

--- scfw/frank_wolfe.py
+++ scfw/frank_wolfe.py
@@ -61,7 +61,7 @@ def run_frank_wolfe(problem, x_0=None, alpha_policy='standard', max_iter=1000, e
         lower_bound = max(lower_bound, f - fw_state['Gap'])
         if (lower_bound - upper_bound) / abs(lower_bound) > 1e-10:
             sys.exit("Lower bound bigger than upper bound")
-        real_Gap=upper_bound - lower_bound
+        real_Gap = upper_bound - lower_bound
         criterion = min(criterion, norm(x - x_last) / max(1, norm(x_last)))
         
         if k % print_every == 0 or k == 1:
@@ -69,7 +69,7 @@ def run_frank_wolfe(problem, x_0=None, alpha_policy='standard', max_iter=1000, e
                   f' upper_bound={upper_bound:.2e}, lower_bound={lower_bound:.2e},'
                   f' real_Gap={real_Gap:.2e}, f_val={f}')
 
-        if criterion <= eps and upper_bound - lower_bound/np.abs(lower_bound)<=eps:
+        if (criterion <= eps) and (upper_bound - lower_bound) / np.abs(lower_bound) <= eps:
             f_hist.append(f)
             f = problem.val(x_best)
             print('Convergence achieved!')
